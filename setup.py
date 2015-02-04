c71968aed77130a4617f32d11ffd8e43fa51b012
Richard Jones <r1chardj0n3s@gmail.com>
2015-02-04 15:11:50 +1100

    setup.py tweaks

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='bower.py',
     version='1.0',
-    description='A sample Python project',
+    description='How I learned to stop worrying and love the bower',
     long_description=long_description,
     url='https://github.com/r1chardj0n3s/laughing-robot',
     author='Richard Jones',
     author_email='r1chardj0n3s@gmail.com',
     license='Apache 2.0',
     classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: Apache Software License',
